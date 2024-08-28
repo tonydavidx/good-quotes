38ac0426f76e893c5e3e6f9567d564072030c20e
Tony David <tonydavidx@gmail.com>
2024-08-28 20:14:10 +0530

    basic setup for scrapping all details is ready

--- soupversion copy.py
+++ soupversion copy.py
@@ -4,12 +4,15 @@ from time import sleep
 from random import randint
 from bs4 import BeautifulSoup
 import json
-
 from get_user_agent import get_user_agent
 
-with open('D:/Documents/Python/Scrapping Projects/goodquotes/scrappedQuotePages.csv', 'r', encoding="utf-8") as f:
+scrapped_pages_file = "D:/Documents/Python/Scrapping Projects/goodquotes/data/csv/scrapped_links.csv"
+quotes_toScrap_file = "D:/Documents/Python/Scrapping Projects/goodquotes/data/csv/links_toScrap.csv"
+quotes_file = "D:/Documents/Python/Scrapping Projects/goodquotes/data/quote_data.json"
+
+with open(scrapped_pages_file, 'r', encoding="utf-8") as f:
     scrapped_pages = f.readlines()
-with open("D:/Documents/Python/Scrapping Projects/goodquotes/urls.csv", 'r', encoding="utf-8") as f:
+with open(quotes_toScrap_file, 'r', encoding="utf-8") as f:
     urls = f.readlines()
 
 my_agent = {"User-Agent": get_user_agent()}
@@ -28,14 +31,14 @@ for url in urls:
     num_pages = int(last_page)
 
     # Load existing quotes from JSON file
-    with open('D:/Documents/Python/Scrapping Projects/goodquotes/data/scrappedquotes.json', 'r', encoding='utf-8') as f:
+    with open(quotes_file, 'r', encoding='utf-8') as f:
         existing_quotes = json.load(f)
 
     # List to store new quotes
     new_quotes = []
     print(f"scraping {url} ...")
     # Loop through each page and scrape quotes
-    for i in range(1, num_pages+1):
+    for i in range(2):
         print(f"scrapping page {i} ...", end='\r')
         page_url = f'{url}?page={i}'
         response = requests.get(page_url, headers=my_agent)
@@ -53,21 +56,45 @@ for url in urls:
             quote = quote.split('―')[0]
             author = container.find(
                 'span', {'class': 'authorOrTitle'}).text.strip().replace(',', '')
-            # Check if the quote is less than 300 characters before adding it to the new quotes list
-            if len(quote) < 280:
-                # Check if the quote already exists in the existing quotes list
-                if not any(q['quote'] == quote for q in existing_quotes):
-                    try:
-                        language = detect(quote)
-                        if language == 'en':
-                            # Create a dictionary for the quote data and append to the new quotes list
-                            quote_data = {
-                                'quote': quote,
-                                'author': author,
-                            }
-                            new_quotes.append(quote_data)
-                    except Exception as e:
-                        print(e)
+
+            tags_div = container.find('div', {'class': 'quoteFooter'})
+            tags = tags_div.find_all('a')
+
+            tag_names = [tag.get_text(strip=True) for tag in tags[:-1]]
+            tag_links = ["https://www.goodreads.com"+tag['href']
+                         for tag in tags[:-1]]
+            likes = int(tags[-1].get_text(strip=True).replace(" likes", ""))
+
+            try:
+                quote_book_link = container.find(
+                    'span', id=lambda x: x and x.startswith("quote_book_link"))
+                quote_book_link = quote_book_link.find('a')['href']
+                tag_links.append("https://www.goodreads.com"+quote_book_link)
+                print("https://www.goodreads.com"+quote_book_link)
+                # sleep(15)
+            except Exception as e:
+                pass
+
+            # print(tags_names, likes)
+            # print(tags_links)
+            # sleep(15)
+
+            # Check if the quote already exists in the existing quotes list
+            if not any(q['quote'] == quote for q in existing_quotes):
+                try:
+                    language = detect(quote)
+                    if language == 'en':
+                        # Create a dictionary for the quote data and append to the new quotes list
+                        quote_data = {
+                            'quote': quote,
+                            'author': author,
+                            'likes': likes,
+                            'tags': tag_names,
+                            'tag_links': tag_links
+                        }
+                        new_quotes.append(quote_data)
+                except Exception as e:
+                    print(e)
         # print(new_quotes)
         sleep(randint(3, 15))
 
@@ -76,18 +103,12 @@ for url in urls:
     existing_quotes += new_quotes
 
     # Save quotes to the JSON file
-    with open('D:/Documents/Python/Scrapping Projects/goodquotes/data/scrappedquotes.json', 'w', encoding="utf-8") as f:
-        json.dump(existing_quotes, f)
-
-    with open('D:/Documents/Software-Projects/my-newtab/public/assets/quotes.json', 'w', encoding="utf-8") as f:
-        json.dump(existing_quotes, f)
-
-    with open('D:/Documents/Software-Projects/my-newtab/dist/assets/quotes.json', 'w', encoding="utf-8") as f:
+    with open(quotes_file, 'w', encoding="utf-8") as f:
         json.dump(existing_quotes, f)
 
     scrapped_pages.append(url)
 
-    with open("D:/Documents/Python/Scrapping Projects/goodquotes/scrappedQuotePages.csv", "w", encoding='utf-8') as f:
+    with open(scrapped_pages_file, "w", encoding='utf-8') as f:
         f.writelines(scrapped_pages)
 
     sleep(randint(30, 60))
